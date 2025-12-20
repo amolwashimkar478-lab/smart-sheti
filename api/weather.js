9047f71c470efabbade6d45ebd9ee63f55f4805d
amolwashimkar478-lab <amolwashimkar478@gmail.com>
2025-12-20 16:22:14 +0530

    Update weather.js

--- api/weather.js
+++ api/weather.js
@@ -1,18 +1,20 @@
 export default async function handler(req, res) {
-  try {
-    const { lat, lon } = req.query;
-    const apiKey = process.env.WEATHER_API_KEY;
+  const { lat, lon } = req.query;
 
-    if (!apiKey) {
-      return res.status(500).json({ error: "Weather API key missing" });
-    }
-
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-    const response = await fetch(url);
-    const data = await response.json();
-
-    res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json({ error: "Weather API error" });
+  if (!lat || !lon) {
+    return res.status(400).json({ error: "Lat/Lon missing" });
   }
+
+  const apiKey = process.env.WEATHER_API_KEY;
+
+  if (!apiKey) {
+    return res.status(500).json({ error: "API key missing" });
+  }
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  res.status(200).json(data);
 }
