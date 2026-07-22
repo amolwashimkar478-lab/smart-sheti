2efd67823bd3e75abdd7d70536d27babf8221094
amolwashimkar478-lab <amolwashimkar478@gmail.com>
2026-07-22 10:45:50 +0530

    Update disease.js

--- pages/api/disease.js
+++ pages/api/disease.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        model: "llama-3.2-11b-vision-instruct",
+        model: "llama-3.2-90b-vision-instruct",
         messages: [
           {
             role: "user",
